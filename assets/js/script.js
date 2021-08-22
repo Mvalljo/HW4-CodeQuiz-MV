ebc4f739ec07094705a9c43bc412dca2b7e9f8d4
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-08-21 20:57:08 -0500

    Added HighScore

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ var button3 = document.createElement("button");
 var button4 = document.createElement("button");
 var choicesEl = document.createElement("div");
 var h1E1 = document.createElement("h1");
+var pEl = document.createElement("p");
+var submitBtn = document.createElement("button");
+
 var questions = [
     'Commonly used data types DO NOT include:',
     'The condition in an if/else statement is enclosed within ___.',
@@ -35,6 +38,7 @@ function countdown() {
         timerEl.textContent = timeLeft;
         if (timeLeft === 0) {
             clearInterval(timer);
+            endQuiz();
         }
     }, 1000);
 }
@@ -43,6 +47,7 @@ function countdown() {
 var i = 0;
 var w = 0;
 function nextQ() {
+
     if (i === 0) {
         quiz.innerHTML = '';
         h1E1.textContent = questions[0];
@@ -54,7 +59,7 @@ function nextQ() {
         button2.addEventListener("click", wrongChoice);
         button3.addEventListener("click", rightChoice);
         button4.addEventListener("click", wrongChoice);
-      
+
     } else if (i === 1) {
         quiz.innerHTML = '';
         h1E1.textContent = questions[1];
@@ -67,7 +72,6 @@ function nextQ() {
         button3.addEventListener("click", rightChoice);
         button4.addEventListener("click", wrongChoice);
 
-        
     } else if (i === 2) {
         quiz.innerHTML = '';
         h1E1.textContent = questions[2];
@@ -79,7 +83,6 @@ function nextQ() {
         button2.addEventListener("click", wrongChoice);
         button3.addEventListener("click", wrongChoice);
         button4.addEventListener("click", rightChoice);
-
 
     } else if (i === 3) {
         quiz.innerHTML = '';
@@ -93,8 +96,6 @@ function nextQ() {
         button3.addEventListener("click", rightChoice);
         button4.addEventListener("click", wrongChoice);
 
-        
-
     } else if (i === 4) {
         quiz.innerHTML = '';
         h1E1.textContent = questions[4];
@@ -107,42 +108,99 @@ function nextQ() {
         button3.addEventListener("click", wrongChoice);
         button4.addEventListener("click", rightChoice);
 
+    } else {
+        endQuiz();
     }
     h1E1.setAttribute("style", "width:600px; text-align:left;");
-    quiz.appendChild(h1E1);
     choicesEl.appendChild(button1);
     choicesEl.appendChild(button2);
     choicesEl.appendChild(button3);
     choicesEl.appendChild(button4);
+
+    quiz.appendChild(h1E1);
+    quiz.appendChild(choicesEl);
 
     button1.setAttribute("style", "margin:5px; width:fit-content;");
     button2.setAttribute("style", "margin:5px; width:fit-content;");
     button3.setAttribute("style", "margin:5px; width:fit-content;");
     button4.setAttribute("style", "margin:5px; width:fit-content;");
     choicesEl.setAttribute("style", "display:flex; flex-direction:column; width:20%;");
-    quiz.appendChild(choicesEl);
-   
+
+
 }
+
 var correctAnsMsg = document.createElement("p");
 var wrongAnsMsg = document.createElement("p");
+
 function rightChoice() {
     i++;
     nextQ();
     correctAnsMsg.textContent = "Correct!";
+    setTimeout(function () {
+        correctAnsMsg.textContent = "";
+    }, 2000);
     correctAnsMsg.setAttribute("style", "font-style:italic; text-align:left; border-top:1px solid black;");
-    quiz.appendChild(correctAnsMsg);
+    choicesEl.appendChild(correctAnsMsg);
 }
 
 function wrongChoice() {
-    nextQ();
     if (timeLeft >= 15) {
-        timeLeft -=15;
+        timeLeft -= 15;
+        nextQ();
+        wrongAnsMsg.textContent = "Wrong!";
+        setTimeout(function () {
+            wrongAnsMsg.textContent = "";
+        }, 1000);
+
+    } else if (timeLeft <= 15) {
+        endQuiz();
     }
-    
-    wrongAnsMsg.textContent = "Wrong!";
     wrongAnsMsg.setAttribute("style", "font-style:italic; text-align:left; border-top:1px solid black;");
-    quiz.appendChild(wrongAnsMsg);
-    w++;
+    choicesEl.appendChild(wrongAnsMsg);
+}
+var divEl = document.createElement("div")
+var y = document.createElement("p")
+var x = document.createElement("input");
+
+function endQuiz() {
+    quiz = '';
+    choicesEl.remove();
+    h1E1.textContent = 'All Done!';
+    clearTimeout(timer);
+    pEl.textContent = 'Your Score is ' + timeLeft;
+    h1E1.appendChild(pEl);
+
+    y.textContent = 'Enter Initials: ';
+    divEl.appendChild(y);
+    y.setAttribute("style", "font-weight: lighter;display: inline-block;");
+
+    x.setAttribute("type", "text");
+    x.setAttribute("value", "");
+    divEl.appendChild(x);
+    submitBtn.textContent = 'Submit';
+    divEl.appendChild(submitBtn);
+    h1E1.appendChild(divEl);
+    submitBtn.addEventListener("click", highScore);
+
 }
 
+function highScore() {
+    document.body.innerHTML = '';
+    var h2E1 = document.createElement("h2");
+    h2E1.textContent = "HighScore";
+    h2E1.setAttribute("style", "text-align:center; font-size:30px;")
+    document.body.appendChild(h2E1);
+    var scoreList = document.createElement("p")
+    var score = {
+        initials: x.value,
+        userScore: timeLeft,
+    };
+    localStorage.setItem("score", JSON.stringify(score));
+    var lastScore = JSON.parse(localStorage.getItem("score"));
+    if (lastScore !== null) {
+        scoreList.textContent ="1." + lastScore.initials +
+            " - " + lastScore.userScore
+    }
+    h2E1.appendChild(scoreList);
+}
 startButton.addEventListener("click", startQuiz);
